ee189d5a33a61f4b2b2d9e29db73f23463fce131
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-10-19 01:35:03 -0400

    Add Block Hovers

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ var button = document.getElementsByTagName('button');
 
 var locationsDiv = document.getElementsByClassName('hide-div');
 
+var box = document.getElementsByTagName('article');
+
 
 function changeColor() {
 
 	this.classList.toggle('colorjs')
+	
+};
+
+function changeOpacity() {
+
+	this.classList.toggle('boxhover');
 	
 };
 
@@ -21,6 +29,12 @@ for(i=0; i < button.length; i++){
 	button[i].addEventListener('mouseenter', changeColor);
 	button[i].addEventListener('mouseleave', changeColor);
 	button[i].addEventListener('click', showDiv);
+
 };
 
+for(i=0; i < box.length; i++){
 
+	box[i].addEventListener('mouseenter', changeOpacity);
+	box[i].addEventListener('mouseleave', changeOpacity);
+
+};
